18d03ba3ddd54d43d182932f3e88fd7d8523cb60
Alister MacCormack <78695941+a-maccormack@users.noreply.github.com>
2025-11-22 20:58:28 -0300

    add ws timeout

--- infra/core_api/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "[ENTRYPOINT] Running Migrations"
-uv run alembic upgrade head
-
-echo "[ENTRYPOINT] Running FastAPI"
-uv run uvicorn src.main:app --host 0.0.0.0 --port 9000 --timeout 300
